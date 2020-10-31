322530732e20c130349a16ccf8e9ebaad4c86dd0
dcumelles <didac.cumelles.7e3@itb.cat>
2020-10-31 02:08:34 +0100

    hint

--- app/src/main/java/cat/itb/geoguesser/FlagArray.java
+++ app/src/main/java/cat/itb/geoguesser/FlagArray.java
@@ -7,26 +7,60 @@ public class FlagArray {
     private int position=0;
 
     public FlagModel[] flags = new FlagModel[]{
-            new FlagModel("Andorra", R.drawable.andorra),
-            new FlagModel("Argentina", R.drawable.argentina),
-            new FlagModel("Belgium", R.drawable.belgium),
-            new FlagModel("Brazil", R.drawable.brazil),
-            new FlagModel("Chile", R.drawable.chile),
-            new FlagModel("China", R.drawable.china),
-            new FlagModel("Colombia", R.drawable.colombia),
-            new FlagModel("Cuba", R.drawable.cuba),
-            new FlagModel("Czech Republic", R.drawable.czech_republic),
-            new FlagModel("Denmark", R.drawable.denmark),
-            new FlagModel("Ecuador", R.drawable.ecuador),
-            new FlagModel("Egypt", R.drawable.egypt),
-            new FlagModel("El Salvador", R.drawable.el_salvador),
-            new FlagModel("Ethiopia", R.drawable.ethiopia),
-            new FlagModel("Finland", R.drawable.finland),
-            new FlagModel("France", R.drawable.france),
-            new FlagModel("Germany", R.drawable.germany),
-            new FlagModel("Greece", R.drawable.greece),
-            new FlagModel("Hawaii", R.drawable.hawaii),
-            new FlagModel("Hungary", R.drawable.hungary),
+            new FlagModel("andorra", R.drawable.andorra),
+            new FlagModel("argentina", R.drawable.argentina),
+            new FlagModel("belgium", R.drawable.belgium),
+            new FlagModel("brazil", R.drawable.brazil),
+            new FlagModel("chile", R.drawable.chile),
+            new FlagModel("china", R.drawable.china),
+            new FlagModel("colombia", R.drawable.colombia),
+            new FlagModel("cuba", R.drawable.cuba),
+            new FlagModel("czech Republic", R.drawable.czech_republic),
+            new FlagModel("denmark", R.drawable.denmark),
+            new FlagModel("ecuador", R.drawable.ecuador),
+            new FlagModel("egypt", R.drawable.egypt),
+            new FlagModel("el Salvador", R.drawable.el_salvador),
+            new FlagModel("ethiopia", R.drawable.ethiopia),
+            new FlagModel("finland", R.drawable.finland),
+            new FlagModel("france", R.drawable.france),
+            new FlagModel("germany", R.drawable.germany),
+            new FlagModel("greece", R.drawable.greece),
+            new FlagModel("hawaii", R.drawable.hawaii),
+            new FlagModel("hungary", R.drawable.hungary),
+            new FlagModel("india", R.drawable.india),
+            new FlagModel("iraq", R.drawable.iraq),
+            new FlagModel("ireland", R.drawable.ireland),
+            new FlagModel("italy", R.drawable.italy),
+            new FlagModel("jamaica", R.drawable.jamaica),
+            new FlagModel("japan", R.drawable.japan),
+            new FlagModel("lebanon", R.drawable.lebanon),
+            new FlagModel("malta", R.drawable.malta),
+            new FlagModel("mexico", R.drawable.mexico),
+            new FlagModel("nepal", R.drawable.nepal),
+            new FlagModel("netherlands", R.drawable.netherlands),
+            new FlagModel("north korea", R.drawable.north_korea),
+            new FlagModel("peru", R.drawable.peru),
+            new FlagModel("philippines", R.drawable.philippines),
+            new FlagModel("poland", R.drawable.poland),
+            new FlagModel("portugal", R.drawable.portugal),
+            new FlagModel("puerto rico", R.drawable.puerto_rico),
+            new FlagModel("qatar", R.drawable.qatar),
+            new FlagModel("romania", R.drawable.romania),
+            new FlagModel("senegal", R.drawable.senegal),
+            new FlagModel("slovenia", R.drawable.slovenia),
+            new FlagModel("south africa", R.drawable.south_africa),
+            new FlagModel("south korea", R.drawable.south_korea),
+            new FlagModel("spain", R.drawable.spain),
+            new FlagModel("sweden", R.drawable.sweden),
+            new FlagModel("switzerland", R.drawable.switzerland),
+            new FlagModel("syria", R.drawable.syria),
+            new FlagModel("ukraine", R.drawable.ukraine),
+            new FlagModel("united kingdom", R.drawable.united_kingdom),
+            new FlagModel("uruguay", R.drawable.uruguay),
+            new FlagModel("usa", R.drawable.usa),
+            new FlagModel("venezuela", R.drawable.venezuela),
+            new FlagModel("wales", R.drawable.wales),
+
     };
 
     public FlagModel[] createFlagArray(int size) {
@@ -56,10 +90,6 @@ public class FlagArray {
 
     public static int random(int min,int max){
         return (int)Math.floor(Math.random()*(max-min+1)+(min));
-    }
-
-    public int listSize(){
-        return flags.length;
     }
 
     public String randomName(){
--- app/src/main/java/cat/itb/geoguesser/MainActivity.java
+++ app/src/main/java/cat/itb/geoguesser/MainActivity.java
@@ -1,22 +1,38 @@
 package cat.itb.geoguesser;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private final int ARRAY_SIZE = 10;
+    private final int ARRAY_SIZE = 30;
+    private int hints = 3;
     private ImageView image;
-    private Button b_1, b_2, b_3, b_4;
+    private TextView tv_result, tv_score, tv_counter;
+    private ProgressBar pb;
+    private Button b_1, b_2, b_3, b_4, b_hint;
     private final FlagArray flagArray = new FlagArray();
-    private final FlagModel[] flags=flagArray.createFlagArray(ARRAY_SIZE);
+    private final FlagModel[] flags = flagArray.createFlagArray(ARRAY_SIZE);
     private Button[] buttons;
-    boolean[] options;
+    private double score = 0;
+    private boolean usedHint = false;
 
 
     @Override
@@ -26,9 +42,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
-        getSupportActionBar().setTitle("Guess The Flag!!");
+        getSupportActionBar().setTitle("Which Flag Is It?");
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        tv_result = findViewById(R.id.tv_result);
+        tv_score = findViewById(R.id.tv_score);
+        tv_counter = findViewById(R.id.tv_counter);
+        pb = findViewById(R.id.pb);
+        b_hint = findViewById(R.id.b_hint);
+        b_hint.setText("HINT\n" + hints);
 
         b_1 = findViewById(R.id.b_1);
         b_1.setOnClickListener(this);
@@ -38,10 +61,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         b_3.setOnClickListener(this);
         b_4 = findViewById(R.id.b_4);
         b_4.setOnClickListener(this);
-        image = findViewById(R.id.flag);
-        buttons = new Button[]{b_1,b_2,b_3,b_4};
-        update();
+        b_hint.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                if (hints != 0) {
+                    hints -= 1;
+                    b_hint.setText("HINT\n" + hints);
+                    usedHint = true;
 
+                    Toast.makeText(MainActivity.this, "hint", Toast.LENGTH_SHORT).show();
+                    for (Button button : buttons) {
+                        if (button.getText().equals(getCurrentName(1))) {
+                            button.setTextColor(getResources().getColor(R.color.colorPrimary));
+                        }
+                    }
+
+                    if (hints == 0){
+                        b_hint.setEnabled(false);
+                    }
+
+                }
+            }
+        });
+        image = findViewById(R.id.flag);
+        buttons = new Button[]{b_1, b_2, b_3, b_4};
+        update();
 
 
     }
@@ -49,41 +94,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Button b =(Button) v;
+        Button b = (Button) v;
         check(b);
     }
 
-    public void check(Button b){
-        if (b.getText() == flags[flagArray.getPosition()-1].getName()) {
-            Toast.makeText(this, b.getText(), Toast.LENGTH_SHORT).show();
+    public void check(Button b) {
+        if (b.getText() == flags[flagArray.getPosition() - 1].getName()) {
+            tv_result.setText("CORRECT");
+            tv_result.setTextColor(getResources().getColor(R.color.colorPrimary));
+            if (!usedHint) {
+                score++;
+            } else {
+                usedHint = false;
+            }
 
-
+        } else {
+            tv_result.setText("WRONG!");
+            tv_result.setTextColor(getResources().getColor(R.color.red));
+            score -= 0.5;
         }
 
         update();
     }
 
-    public void update(){
-
-
-        if (flagArray.getPosition()!=ARRAY_SIZE) {
-            int r=FlagArray.random(0,3);
-            options=new boolean[]{false,false,false,false};
-            options[r]=true;
-            for (int i = 0; i<buttons.length; i++) {
-                if (options[i]){
-                    buttons[i].setText(flags[flagArray.getPosition()].getName());
-
-                }else {
-                    buttons[i].setText(flagArray.randomName());
+    public void update() {
+        if (flagArray.getPosition() != ARRAY_SIZE) {
+            for (Button button : buttons) {
+                button.setText("");
+                button.setTextColor(getResources().getColor(R.color.black));
+            }
+            List<String> added = new ArrayList<>();
+            int r = FlagArray.random(0, 3);
+            String ok = getCurrentName(0);
+            buttons[r].setText(ok);
+            added.add(ok);
+            for (Button button : buttons) {
+                boolean repeat = true;
+                String rand = "";
+                while (repeat) {
+                    rand = flagArray.randomName();
+                    if (!added.contains(rand)) {
+                        repeat = false;
+                        added.add(rand);
+                    }
+                }
+                if (!button.getText().equals(ok)) {
+                    button.setText(rand);
                 }
             }
-            setImage();
+
+            image.setImageResource(flags[flagArray.getPosition()].getImage());
             flagArray.nextFlag();
+            String counter = flagArray.getPosition() + "/" + ARRAY_SIZE;
+            tv_counter.setText(counter);
+            String strcore = "SCORE\n" + score;
+            tv_score.setText(strcore);
         }
     }
 
-    public void setImage(){
-        image.setImageResource(flags[flagArray.getPosition()].getImage());
+    public String getCurrentName(int back) {
+        return flags[flagArray.getPosition() - back].getName();
     }
+
 }
